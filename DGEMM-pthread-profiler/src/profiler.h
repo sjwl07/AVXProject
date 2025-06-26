9d627f7d2b6addaef0ac34ecf93abca9901b2bae
sjwl07 <shivamj2005@gmail.com>
2025-06-26 21:13:17 +0000

    DGEMM with updated profiler

---
+++ DGEMM-pthread-profiler/src/profiler.h
@@ -0,0 +1,11 @@
+#ifndef PROFILER_H
+#define PROFILER_H
+
+// Function to start the profiling thread
+void profiler_start();
+
+// Function to stop the profiling thread and write results
+void profiler_stop();
+
+#endif // PROFILER_H
+
